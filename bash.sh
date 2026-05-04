8abc9c6e78f23d1255878623f86e65a85f671df8
pers5124wastaken <pers5124@trollmeight.me>
2026-05-03 22:15:30 -0700

    kill me

--- bash.sh
+++ bash.sh
@@ -67,3 +67,4 @@ elif [ $option = 69 ]; then
 curl https://raw.githubusercontent.com/ferrochrome-3/nikkis-vt2-utility/refs/heads/main/femboy%20ascii.sh > "femboy ascii.sh" && bash "femboy ascii.sh"
 fi
 fi
+fi
